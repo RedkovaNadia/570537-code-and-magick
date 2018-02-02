f58955d1cecbbcc000664a570ce78ee3ce933215
NadiaRedkova <nadia.redkova@mail.ru>
2018-02-02 03:57:20 -0800

    Добавила переменные, чтобы сократить вычисления

--- js/stat.js
+++ js/stat.js
@@ -11,6 +11,11 @@ var MAX_BAR_HEIGHT = 150;
 var GAP_BETWEEN_BARS = 50;
 var MAX_BAR_Y = CLOUD_Y + GAP * 4 + FONT_GAP * 3; // координаты по у где может начинаться колонка с максимальной высотой
 var NOTE_OF_MAX_TIME_Y = MAX_BAR_Y - GAP; // координаты по у где может начинаться запись для колонки с максимальной высотой
+var VICTORY_TEXT_X = CLOUD_X + GAP * 2;
+var VICTORY_TEXT_Y = CLOUD_Y + GAP * 2 + FONT_GAP;
+var NAMES_Y = CLOUD_Y + GAP * 6 + FONT_GAP * 3;
+var TOTAL_GAP = (BAR_WIDTH + GAP_BETWEEN_BARS);
+var NOTE_AND_BAR_X = CLOUD_X + GAP * 3;
 
 
 var renderCloud = function (ctx, x, y, color) {
@@ -39,20 +44,21 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2 + FONT_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3 + FONT_GAP * 2);
+  ctx.fillText('Ура вы победили!', VICTORY_TEXT_X, VICTORY_TEXT_Y);
+  ctx.fillText('Список результатов:', VICTORY_TEXT_X, VICTORY_TEXT_Y + GAP + FONT_GAP);
 
   var maxTime = getMaxElement(times);
   for (var i = 0; i < names.length; i++) {
+    var currentBarHeight = Math.round(times[i]) * MAX_BAR_HEIGHT / maxTime;
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], CLOUD_X + GAP * 3 + (BAR_WIDTH + GAP_BETWEEN_BARS) * i, CLOUD_Y + GAP * 6 + FONT_GAP * 3 + MAX_BAR_HEIGHT);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP * 3 + (BAR_WIDTH + GAP_BETWEEN_BARS) * i, NOTE_OF_MAX_TIME_Y + (MAX_BAR_HEIGHT - Math.round(times[i]) * MAX_BAR_HEIGHT / maxTime));
+    ctx.fillText(names[i], NOTE_AND_BAR_X + TOTAL_GAP * i, NAMES_Y + MAX_BAR_HEIGHT);
+    ctx.fillText(Math.round(times[i]), NOTE_AND_BAR_X + TOTAL_GAP * i, NOTE_OF_MAX_TIME_Y + MAX_BAR_HEIGHT - currentBarHeight);
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       var rgbaX = getRandomNumber(0.1, 0.9);
       ctx.fillStyle = 'rgba(0, 0, 255, ' + rgbaX + ')';
     }
-    ctx.fillRect(CLOUD_X + GAP * 3 + (BAR_WIDTH + GAP_BETWEEN_BARS) * i, MAX_BAR_Y + (MAX_BAR_HEIGHT - Math.round(times[i]) * MAX_BAR_HEIGHT / maxTime), BAR_WIDTH, Math.round(times[i]) * MAX_BAR_HEIGHT / maxTime);
+    ctx.fillRect(NOTE_AND_BAR_X + TOTAL_GAP * i, MAX_BAR_Y + MAX_BAR_HEIGHT - currentBarHeight, BAR_WIDTH, currentBarHeight);
   }
 };
