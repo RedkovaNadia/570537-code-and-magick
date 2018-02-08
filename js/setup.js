5f953fb9b9dea24f2050c08bbccbde3adb837f7f
NadiaRedkova <nadia.redkova@mail.ru>
2018-02-08 02:04:36 -0800

    Убрала лишние параметры и поправила ошибки линтера

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,7 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)','rgb(0, 0, 0)'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var userDialog = document.querySelector('.setup');
@@ -14,7 +14,7 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template').c
 
 var numberOfWizards = 4;
 
-var generateWizardsArray = function (WIZARD_NAMES, WIZARD_SURNAMES, COAT_COLOR, EYES_COLOR) {
+var generateWizardsArray = function () {
   var wizards = [];
   for (i = 0; i < numberOfWizards; i++) {
     wizards.push({name: WIZARD_NAMES[i] + WIZARD_SURNAMES[i], coatColor: COAT_COLOR[i], eyesColor: EYES_COLOR[i]});
@@ -32,12 +32,12 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var wizardsArray = generateWizardsArray (WIZARD_NAMES, WIZARD_SURNAMES, COAT_COLOR, EYES_COLOR);
+var wizardsArray = generateWizardsArray();
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizardsArray.length; i++) {
   fragment.appendChild(renderWizard(wizardsArray[i]));
-};
+}
 similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
